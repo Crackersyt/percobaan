4439d50707d10c1b62904a6b992916d777d030ac
Crackersyt <denirizki875@gmail.com>
2023-03-06 20:12:46 +0700

    commit pertama

---
+++ resources/views/deni.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>sasdwhwhwhwhwhwh</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem obcaecati natus tempora itaque neque, ex dolorem rem cupiditate ipsa mollitia placeat numquam tempore esse doloribus optio nam deleniti iusto reprehenderit?</p>
+</body>
+</html>
